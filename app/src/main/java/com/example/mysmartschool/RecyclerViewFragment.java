b6eb430fa44ee3d7ef1cd8ba24130a5f6e2c561f
HafizhS <methafizh30@gmail.com>
2020-01-22 12:53:38 +0700

    Add RecyclerView Only Fragment

---
+++ app/src/main/java/com/example/mysmartschool/RecyclerViewFragment.java
@@ -0,0 +1,30 @@
+package com.example.mysmartschool;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewFragment<VH extends RecyclerView.ViewHolder> extends Fragment {
+
+    private String title;
+    private RecyclerView.Adapter<VH> adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    public RecyclerViewFragment(String title, RecyclerView.LayoutManager layoutManager, RecyclerView.Adapter adapter) {
+        this.title = title;
+        this.adapter = adapter;
+        this.layoutManager = layoutManager;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recyclerview, container, false);
+    }
+}
